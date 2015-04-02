926ec72764d9e1f6ead8a5c936b527972cccd42e
Brandon Lilly <brandonjlilly@gmail.com>
2015-04-02 11:14:51 -0700

    initialize repository

---
+++ app/assets/javascripts/news_reader.js
@@ -0,0 +1,13 @@
+window.NewsReader = {
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  initialize: function() {
+    console.log('Hello from Backbone!');
+  }
+};
+
+$(document).ready(function(){
+  NewsReader.initialize();
+});
